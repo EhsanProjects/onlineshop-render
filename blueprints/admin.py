38887840fd79c24ed587c019e5c8660b7980f578
unknown <ehsan.heidarian2020@gmail.com>
2023-08-15 15:15:57 -0500

    Admin page/Secret_key added

--- blueprints/admin.py
+++ blueprints/admin.py
@@ -1,7 +1,31 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, session, redirect, abort
+
+import config
+
 app = Blueprint("admin", __name__)
 
-@app.route('/admin')
-def admin():  # put application's code here
-    return 'admin page!'
 
+@app.route('/admin/login', methods=["POST", "GET"])
+def admin():  # put application's code here
+    if request.method == "POST":
+        username = request.form.get('username', None)
+        password = request.form.get('password', None)
+
+        if username == config.ADMIN_USERNAME and password == config.ADMIN_PASSWORD:
+            # This shows what session is active now. By checking its value
+            session['admin_login'] = username
+            return redirect("/admin/dashboard")
+        else:
+            redirect("/admin/login")
+    else:
+
+        return render_template("/admin/login.html")
+
+
+@app.route('/admin/dashboard', methods=["GET"])
+def dashboard():
+    if session.get('admin_login', None) == None:
+        return "Error: Not logged in yet"
+        # abort(403)
+
+    return "dashboard"
--- config.py
+++ config.py
@@ -1,1 +1,12 @@
+import os
+
 SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
+ADMIN_USERNAME = "admin"
+ADMIN_PASSWORD = "1"
+
+
+def generate_secret_key(length=32):
+    return os.urandom(length).hex()
+
+
+SECRET_KEY = generate_secret_key()
