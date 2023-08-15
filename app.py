3804a3a79ac4effbbc525d6e8e10a79e9af4889e
unknown <ehsan.heidarian2020@gmail.com>
2023-08-15 00:51:45 -0500

    database added+ 2 table/ready to use

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 from flask import Flask
+
+
+
 from blueprints.general import app as general
 from blueprints.admin import app as admin
 from blueprints.user import app as user
+import config
+import extensions
+
 
 app = Flask(__name__)
 app.register_blueprint(general)
 app.register_blueprint(admin)
 app.register_blueprint(user)
+# db=SQLAlchemy(app)
 
+
+app.config["SQLALCHEMY_DATABASE_URI"] = config.SQLALCHEMY_DATABASE_URI
+extensions.db.init_app(app)
+
+with app.app_context():
+    extensions.db.create_all()
 
 
 if __name__ == '__main__':
