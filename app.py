eccd944686a00a4d9fec12654f1012f95a8385a8
unknown <ehsan.heidarian2020@gmail.com>
2023-09-08 18:49:08 -0500

    User Register Added

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ from blueprints.general import app as general
 from blueprints.admin import app as admin
 from blueprints.user import app as user
 
+from flask_login import LoginManager
+
+from models.user import User
+
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = config.SQLALCHEMY_DATABASE_URI
 app.config['SECRET_KEY'] = config.SECRET_KEY
@@ -14,7 +18,17 @@ extensions.db.init_app(app)
 app.register_blueprint(general)
 app.register_blueprint(admin)
 app.register_blueprint(user)
+
+
 csrf = CSRFProtect(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+@login_manager.user_loader
+def load_user(user_id):
+        # return User.get(user_id) or next line
+        return User.query.filter(User.id == user_id).first()
+
 with app.app_context():
     extensions.db.create_all()
 
--- blueprints/user.py
+++ blueprints/user.py
@@ -1,7 +1,27 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect
+from flask_login import login_user
+from passlib.hash import sha256_crypt
+from extensions import db
+from models.user import User
 import models.user
 app = Blueprint("user", __name__)
 
-@app.route('/user')
-def user():  # put application's code here
-    return 'user page!'
+@app.route('/user/login', methods=['GET', 'POST'])
+def login():  # put application's code here
+    if request.method == 'GET':
+        return render_template('user/login.html')
+    else:
+        register = request.form.get('register', None)
+        username = request.form.get('username', None)
+        password = request.form.get('password', None)
+        phone = request.form.get('phone', None)
+        address = request.form.get('address', None)
+        if register != None:
+            user = User(username=username, password=sha256_crypt.encrypt(password), phone=phone, address=address)
+            db.session.add(user)
+            db.session.commit()
+            login_user(user)
+
+            return  redirect('/user/dashboard')
+
+        return 'done'
