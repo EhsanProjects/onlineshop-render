d7387400064c6f13fa71fce2fba8908698cec505
unknown <ehsan.heidarian2020@gmail.com>
2023-09-09 12:53:05 -0500

    login and register user modified

--- blueprints/user.py
+++ blueprints/user.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_user
 from passlib.hash import sha256_crypt
 from extensions import db
@@ -17,11 +17,24 @@ def login():  # put application's code here
         phone = request.form.get('phone', None)
         address = request.form.get('address', None)
         if register != None:
+            user = User.query.filter(User.username == username).first()
+            if user != None:
+                flash('That username is taken. Try another.')
+                return redirect(url_for('user.login'))
+
             user = User(username=username, password=sha256_crypt.encrypt(password), phone=phone, address=address)
             db.session.add(user)
             db.session.commit()
             login_user(user)
-
             return  redirect('/user/dashboard')
-
-        return 'done'
+        else:
+            user = User.query.filter(User.username == username).first()
+            if user == None:
+                flash('Wrong password or username. Try again or click Forgot password to reset it.')
+                return redirect(url_for('user.login'))
+        if sha256_crypt.verify(password, user.password):
+            login_user(user)
+            return  redirect('/user/dashboard')
+        else:
+            flash('Wrong password or username. Try again or click Forgot password to reset it.')
+            return redirect(url_for('user.login'))
